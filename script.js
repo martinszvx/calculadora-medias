da44af824b913958cec3ed1ae47ddfdf8d98df91
lucas martins bichara <lucasmartins.b07@gmail.com>
2026-03-30 21:13:21 -0300

    funcionamento do JS

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
     /**
      * ESPAÇO PARA O DESAFIO
      * Instruções: 
@@ -9,11 +8,12 @@
      */
     function resolverDesafio() {
         const outputDiv = document.getElementById('output');
-        
         // --- SEU CÓDIGO COMEÇA AQUI ---
-        
-        // Exemplo de como capturar:
-        // let n1 = parseFloat(document.getElementById('n1').value);
-
+        let n1 = parseFloat(document.getElementById('n1').value);
+        let n2 = parseFloat(document.getElementById('n2').value);
+        let n3 = parseFloat(document.getElementById('n3').value);
+        let n4 = parseFloat(document.getElementById('n4').value);
+        let media = (n1+n2+n3+n4)/4;
+        outputDiv.innerText = "A media é: " + media;
         // --- SEU CÓDIGO TERMINA AQUI ---
     }
